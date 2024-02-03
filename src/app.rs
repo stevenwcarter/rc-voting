3795074d391bc2196890a1193ec9a309ee6cc0ad
Steven Carter <steve@javapl.us>
2024-02-03 14:15:01 -0500

    Adding tailwind

--- src/app.rs
+++ src/app.rs
@@ -9,10 +9,6 @@ pub fn App() -> impl IntoView {
     provide_meta_context();
 
     view! {
-
-
-        // injects a stylesheet into the document <head>
-        // id=leptos means cargo-leptos will hot-reload this stylesheet
         <Stylesheet id="leptos" href="/pkg/rc-voting-leptos.css"/>
 
         // sets the document title
@@ -22,12 +18,9 @@ pub fn App() -> impl IntoView {
         <Router fallback=|| {
             let mut outside_errors = Errors::default();
             outside_errors.insert_with_default_key(AppError::NotFound);
-            view! {
-                <ErrorTemplate outside_errors/>
-            }
-            .into_view()
+            view! { <ErrorTemplate outside_errors/> }.into_view()
         }>
-            <main>
+            <main class="my-0 mx-auto max-w-3xl text-center">
                 <Routes>
                     <Route path="" view=HomePage/>
                 </Routes>
@@ -44,7 +37,13 @@ fn HomePage() -> impl IntoView {
     let on_click = move |_| set_count.update(|count| *count += 1);
 
     view! {
-        <h1>"Welcome to Leptos!"</h1>
-        <button on:click=on_click>"Click Me: " {count}</button>
+        <h1 class="p-6 text-4xl text-blue">"Welcome to Leptos!"</h1>
+        <button
+            class="bg-sky-600 hover:bg-sky-700 px-5 py-3 text-white rounded-lg"
+            on:click=on_click
+        >
+            "Click Me: "
+            {count}
+        </button>
     }
 }
